9875c039a771db62e09f87b9948137ed42f0ecfb
Kieu Tran <kieutran@maxflowtech.com>
2026-03-25 21:41:27 +0700

    Update code for deployment

--- src/main/java/com/project/web_clone/config/WebConfig.java
+++ src/main/java/com/project/web_clone/config/WebConfig.java
@@ -1,20 +1,25 @@
 package com.project.web_clone.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Paths;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.upload-dir:./uploads}")
+    private String uploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/assets/_nuxt/**")
                 .addResourceLocations("classpath:/static/_nuxt/");
 
+        String absolutePath = Paths.get(uploadDir).toAbsolutePath().toUri().toString();
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("classpath:/static/uploads/")
-                .addResourceLocations("file:./web-clone/src/main/resources/static/uploads/")
-                .addResourceLocations("file:./src/main/resources/static/uploads/");
+                .addResourceLocations(absolutePath);
     }
 }
